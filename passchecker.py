337fd6d55cfc64a8ff2d51708edc1cf1cdcc583c
Michael Shell <bvg2003@bk.ru>
2021-02-04 03:40:05 +0300

    request_api_data()

---
+++ passchecker.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching API data: {res.status_code}')
+
+
+def pwned_api_check(password):
+    pass
